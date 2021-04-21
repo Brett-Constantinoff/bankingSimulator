e249c987b9dc39f1ca84d7b1c18f6838eedee857
Brett Constantinoff <77081808+Brett-Constantinoff@users.noreply.github.com>
2021-04-21 11:43:44 -0600

    Update bankManager.h

--- bankManager.h
+++ bankManager.h
@@ -1,44 +1,128 @@
-#include <stdlib.h>
-#include <stdio.h>
+//
+// Created by Brett Constantinoff on 2021-04-18.
+//
+#ifndef BANKMANAGER_SYSTEM_H
+#define BANKMANAGER_SYSTEM_H
 
-#define PWRDSIZE 25
-#define MINPWD 8
-#define MAXNAME 50
-#define FILENAME "bankAccounts"
-
+#define MAXTRANS 50
+#define NAMELEN 25
+#define TIMELEN 35
+#define PASSLEN 15
+#define FILENAME  "clients.dat"
+#define SLEEP 2
 
 typedef struct{
-    int accountID;
+    float tAmount;
+    char time[TIMELEN]; //time each transaction occurred
+    unsigned int account; //0 for checking, 1 for savings
+    unsigned int type; //0 for sending, 1 for adding
+}Transaction;
+
+typedef struct{
     float checkings;
     float savings;
-    char password[PWRDSIZE + 1];
-    char name[MAXNAME + 1];
-}bankAccount;
+    Transaction accountTrans[MAXTRANS];
+    unsigned int numTrans;
+}Account;
 
-void *myMalloc(size_t nbytes);
+typedef struct{
+    unsigned long phoneNumber;
+    char clientName[NAMELEN];
+    char password[PASSLEN];
+    char time[TIMELEN]; //time account is created
+    Account accounts;
+}Client;
 
-FILE *openFile(char *fileName, char *mode);
+/*
+ Creates a new client and returns
+ the struct with the new information in
+ it
+*/
+void addClient(void);
 
-void createAccount(bankAccount *account);
+/*
+ * Prints out all current clients in the
+ * system and their information
+ */
+void printClients(void);
 
-void createPassword(bankAccount *account);
+/*
+ * Logs into a current client account
+ * based off phone number and password
+ */
+Client *login(void);
 
-bankAccount verfiyPassword(void);
+/*
+ * Given a password, it will
+ * return 0 or 1 if the given
+ * password does not meet the requirements
+ */
+int verifyPassword(char *password);
 
-void printAccounts(void);
+/*
+ * Opens a given file in a given mode
+ */
+void *openFile(char *fileName, char *mode);
 
-void printAccountInfo(bankAccount account);
+/*
+ * Reads input into the given string
+ */
+void readIntoString(char *string, int stringLen);
 
-void addFunds(bankAccount *account);
+/*
+ * Update clients name
+ */
+void updateClientName(char *name, char *fileName);
 
-void changePassword(bankAccount account);
+/*
+ * Updates a clients phone number
+ */
+void updateClientPhone(unsigned long phone, char *fileName);
 
-void deleteAccount(bankAccount account);
+/*
+ * Displays main interface menu
+ */
+void displayMainMenu(void);
 
-void updateAccountOwner(bankAccount *account);
+/*
+ * Displays menu for editing
+ * client information
+ */
+void displaySubMenu1(void);
 
-bankAccount *getAccounts(char *fileName);
+/*
+ * Adds a user entered amount to an
+ * account of their choice
+ */
+void addFunds(Client *c);
 
-int getNumAccounts(char *fileName);
+/*
+ * Withdraws a user selected amount
+ * of money from their account
+ */
+void withdrawFunds(Client *c);
 
-void saveData(char *fileName, bankAccount *account);
+/*
+ * Displays transactions for current account
+ */
+void displayTransactions(Client *c);
+
+/*
+ * Gets a timestamp and stores it in the given
+ * string
+ */
+void getTime(char *time, int timeLen);
+
+/*
+ * Transfers funds from the given client
+ * to any other client in system
+ */
+void transferFunds(Client *c);
+
+/*
+ * Adds a transaction to a clients list
+ * of transactions
+ */
+void addTransaction(Client *c, int type, int account, float amount);
+#endif //BANKMANAGER_SYSTEM_H
+
